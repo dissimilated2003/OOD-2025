e63dccff1b397d820f4021c05d9ae7cd4de78980
Andrey <159928617+dissimilated2003@users.noreply.github.com>
2026-02-17 22:28:57 +0300

    могущественный автомат со жвачкой

---
+++ Lab8/GumBallMachine/GumBallMachine.cpp
@@ -0,0 +1,24 @@
+﻿#include "GumBallMachine.h"
+
+
+auto Run() -> void
+{
+    using namespace with_state;
+    GumballMachine t{ 10 };
+    std::cout << t.ToString();
+
+    t.EjectQuarter();   //fail
+    t.TurnCrank();      //fail
+    t.InsertQuarter();  //good
+    t.EjectQuarter();   //good
+    t.InsertQuarter();  //good
+
+    t.InsertQuarter();  //fail
+    t.InsertQuarter();  //fail
+    t.TurnCrank();      //good
+}
+
+int main()
+{
+
+}
